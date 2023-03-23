2df223b333d4375cfb30fb12133153519a499860
allegria25 <allegria.kinzola@2026.ulc-icam.com>
2023-03-23 16:59:11 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const key = 'adc297d9a76b275a088913a61975b642'
+ 
+let lien = `https://api.openweathermap.org/data/2.5/weather?q=kinshasa&appid=${key}&units=metric&lang=fr`
+    fetch(lien).then((response) => response.json().then((data) => {console.log(data);
+        document.querySelector('.my-ville').innerHTML = data.name
+        document.querySelector('.temperature').innerHTML = data.main.temp + "°"
+        document.querySelector('.humidite').innerHTML = data.main.humidity +"%"
+        document.querySelector('.vent').innerHTML = data.wind.speed +"km/h"
+    
+    })).catch((e)=> console.log("erreur  : "  + e))
+
+
+        document.querySelector('form').addEventListener('submit', function(e){
+            e.preventDefault();
+            let ville = document.querySelector('#inputcity').value
+
+            const key = 'adc297d9a76b275a088913a61975b642'
+ 
+            let lien = `https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=${key}&units=metric&lang=fr`
+                fetch(lien).then((response) => response.json().then((data) => {console.log(data);
+                    document.querySelector('.my-ville').innerHTML = data.name
+                    document.querySelector('.temperature').innerHTML = data.main.temp + "°"
+                    document.querySelector('.humidite').innerHTML = data.main.humidity +"%"
+                    document.querySelector('.vent').innerHTML = data.wind.speed +"km/h"
+                
+                })).catch((e)=> console.log("erreur  : "  + e))
+            
+        })
+    
+    
+
+
+
+
+
+
+
+
+
+
---
+++ mode.js
@@ -0,0 +1,10 @@
+let change_mode = false
+document.querySelector('.fa-moon').addEventListener('onclick', ()=>{
+    if(change_mode){
+        change_mode = false
+        
+    }else{
+        change_mode = true
+
+    }
+})
