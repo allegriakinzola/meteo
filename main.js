2ea32d2cf916e8ae4e0b461f522a284d9cec3ea0
allegria25 <allegria.kinzola@2026.ulc-icam.com>
2024-01-20 23:22:55 +0100

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,47 @@
 
+
+
+const key = 'adc297d9a76b275a088913a61975b642'
+const inputValue = ''
+async function getData (ville){
+    let link = `https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=${key}&units=metric&lang=fr`
+    try {
+        const response = await fetch(link)
+        const data = await response.json()
+        changeData(data)
+    } catch (error) {
+        console.error("une erreur est survenu lors de la récupération des données")
+    }
+}
+function changeData (data){
+    if(data){
+        document.querySelector('.my-ville').innerHTML = data.name
+        document.querySelector('.temperature').innerHTML = data.main.temp + "°"
+        document.querySelector('.humidite').innerHTML = data.main.humidity +"%"
+        document.querySelector('.vent').innerHTML = data.wind.speed +"km/h"
+    }else{
+        
+    }
+}
+
+document.querySelector('#inputcity').addEventListener('change', function (e) {
+    e.preventDefault();
+    const value = e.target.value;
+    inputValue = value
+    console.log(value);
+  });
+
+document.querySelector('form').addEventListener('submit', function(e){
+    e.preventDefault();
+    const ville = document.querySelector('#inputcity').value
+    getData(inputValue)
+})
+
+
+
+/** 
+ * 
+
 const key = 'adc297d9a76b275a088913a61975b642'
 let call_api = function(ville){
     let lien = `https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=${key}&units=metric&lang=fr`
@@ -13,53 +56,9 @@ let call_api = function(ville){
 
 document.querySelector('form').addEventListener('submit', function(e){
     e.preventDefault();
-    let ville = document.querySelector('#inputcity').value
-    call_api(ville)
+    const ville = document.querySelector('#inputcity').value
+    getData(ville)
 })
 
-call_api('kinshasa')
-/** 
- * 
- * const key = 'adc297d9a76b275a088913a61975b642'
-let lien = `https://api.openweathermap.org/data/2.5/weather?q=kinshasa&appid=${key}&units=metric&lang=fr`
-fetch(lien).then((response) => response.json().then((data) => {console.log(data);
-    document.querySelector('.my-ville').innerHTML = data.name
-    document.querySelector('.temperature').innerHTML = data.main.temp + "°"
-    document.querySelector('.humidite').innerHTML = data.main.humidity +"%"
-    document.querySelector('.vent').innerHTML = data.wind.speed +"km/h"
-
-})).catch((e)=> console.log("erreur  : "  + e))
-
-
-        document.querySelector('form').addEventListener('submit', function(e){
-            e.preventDefault();
-            let ville = document.querySelector('#inputcity').value
-
-            const key = 'adc297d9a76b275a088913a61975b642'
- 
-            let lien = `https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=${key}&units=metric&lang=fr`
-                fetch(lien).then((response) => response.json().then((data) => {console.log(data);
-                    document.querySelector('.my-ville').innerHTML = data.name
-                    document.querySelector('.temperature').innerHTML = data.main.temp + "°"
-                    document.querySelector('.humidite').innerHTML = data.main.humidity +"%"
-                    document.querySelector('.vent').innerHTML = data.wind.speed +"km/h"
-                
-                })).catch((e)=> console.log("erreur  : "  + e))
-            
-        })
  * 
 */
-
-        
-    
-    
-
-
-
-
-
-
-
-
-
-
