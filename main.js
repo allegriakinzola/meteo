c88541b5d9195716d8a6080289e05a0eaa67b91f
allegria25 <allegria.kinzola@2026.ulc-icam.com>
2024-01-21 00:05:35 +0100

    hs

--- main.js
+++ main.js
@@ -1,31 +1,40 @@
 
 
 
-const key = 'adc297d9a76b275a088913a61975b642';
+const key = 'adc297d9a76b275a088913a61975b642'
+const inputValue = ''
+async function getData (ville){
+    let link = `https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=${key}&units=metric&lang=fr`
+    try {
+        const response = await fetch(link)
+        const data = await response.json()
+        changeData(data)
+    } catch (error) {
+        console.error("une erreur est survenu lors de la récupération des données")
+    }
+}
+function changeData (data){
+    document.querySelector('.my-ville').innerHTML = data.name
+    document.querySelector('.temperature').innerHTML = data.main.temp + "°"
+    document.querySelector('.humidite').innerHTML = data.main.humidity +"%"
+    document.querySelector('.vent').innerHTML = data.wind.speed +"km/h"
 
-async function getData(ville) {
-  let link = `https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=${key}&units=metric&lang=fr`;
-  try {
-    const response = await fetch(link);
-    const data = await response.json();
-    changeData(data);
-  } catch (error) {
-    console.error("Une erreur est survenue lors de la récupération des données");
-  }
 }
 
-function changeData(data) {
-  document.querySelector('.my-ville').innerHTML = data.name;
-  document.querySelector('.temperature').innerHTML = data.main.temp + "°";
-  document.querySelector('.humidite').innerHTML = data.main.humidity + "%";
-  document.querySelector('.vent').innerHTML = data.wind.speed + "km/h";
-}
+document.querySelector('#inputcity').addEventListener('change', function (e) {
+    e.preventDefault();
+    const value = e.target.value;
+    inputValue = value
+    console.log(value);
+  });
 
-document.querySelector('form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  const ville = document.querySelector('#inputcity').value;
-  getData(ville);
-});
+document.querySelector('form').addEventListener('submit', function(e){
+    e.preventDefault();
+    const ville = document.querySelector('#inputcity').value
+    getData(ville)
+})
+
+
 
 /** 
  * 
